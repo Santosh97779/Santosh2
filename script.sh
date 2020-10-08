3e5753a66ef91603b264b0e22a0ee4c03c1ef3c0
Santosh97779 <46757351+Santosh97779@users.noreply.github.com>
2020-10-08 11:44:14 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-dpkg --configure -a
-apt-get -y update
-
+#! /bin/bash
 # install Apache2
+echo "Updating packages ..."
 apt-get -y install apache2
 apt-get -y install mysql-server
-apt-get -y install php libapache2-mod-php php-mysql
-# write some HTML
+apt-get -y install php libapache2-mod-php php-mysq
+echo "<h1>Demo Bootstrapping Azure Virtual Machine</h1>" | sudo tee /var/www/html/index.html
 
 # restart Apache
 apachectl restart
